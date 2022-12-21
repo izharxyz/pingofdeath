2ac39c5fd4c3105d056821e117331be65317d8ce
zzedddd <melegend.forever@protonmail.com>
2022-12-21 22:50:44 +0530

    added user register api

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # plugins
+    'rest_framework',
+
+    # custom apps
+    'authapi'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +129,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authapi.User'
