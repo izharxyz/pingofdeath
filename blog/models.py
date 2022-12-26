f9acf3c83d50a11ee91b34a209b75089e6fd306a
zzedddd <melegend.forever@protonmail.com>
2022-12-26 13:53:43 +0530

    added model for blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.contrib.auth.models import User
 
-# Create your models here.
+import uuid
+
+class BaseModel(models.Model):
+    uid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Blog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog')
+    title = models.CharField(
+        max_length=128,
+        validators=[MinLengthValidator(5, 'title must be greater than 5 characters')])
+    body = models.TextField(
+        validators=[MinLengthValidator(100, 'blog must be greater than 100 characters')]
+    )
+    image = models.ImageField(upload_to='blogs_thubnails')
+
+    def __str__(self) -> str:
+        return self.title
