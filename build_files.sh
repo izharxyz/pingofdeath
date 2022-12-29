3368e0fda42a6edd894107036ed9120308ca7527
zzedddd <melegend.forever@protonmail.com>
2022-12-30 01:52:53 +0530

    vercel conf

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,9 @@
 # build_files.sh
-pyhton3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput
+echo "########## BUILD START ##########"
+pyhton3.9 -m pip install -r requirements.txt
+
+python3.9 manage.py makemigrations
+pyhton3.9 manage.py migrate
+
+python3.9 manage.py collectstatic --noinput --clear
+echo "########## BUILD END ##########"
