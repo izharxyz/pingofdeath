e2ad64c2bad541a4ea646ef6eedc4239acc16375
zzedddd <melegend.forever@protonmail.com>
2022-12-30 01:56:08 +0530

    fixed typo

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
 echo "########## BUILD START ##########"
-pyhton3.9 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 python3.9 manage.py makemigrations
 pyhton3.9 manage.py migrate
