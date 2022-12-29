935c8db60e4c5f61d2ae48bf33066238e56b270e
zzedddd <melegend.forever@protonmail.com>
2022-12-30 02:01:28 +0530

    fixed typo

--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,7 @@ python3.9 -m pip install --upgrade setuptools
 python3.9 -m pip install -r requirements.txt
 
 python3.9 manage.py makemigrations
-pyhton3.9 manage.py migrate
+python3.9 manage.py migrate
 
 python3.9 manage.py collectstatic --noinput --clear
 echo "########## BUILD END ##########"
