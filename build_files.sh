5d31de3d03326eded762f9788ae28561f648c3f8
zzedddd <melegend.forever@protonmail.com>
2022-12-30 02:00:02 +0530

    .....

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 # build_files.sh
 echo "########## BUILD START ##########"
+python3.9 -m pip install --upgrade setuptools
 python3.9 -m pip install -r requirements.txt
 
 python3.9 manage.py makemigrations
