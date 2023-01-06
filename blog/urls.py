9fd22ecc670adef1cb8429cfaf13d4ac8f63ce86
zzedddd <melegend.forever@protonmail.com>
2023-01-07 00:23:14 +0530

    added: documentation

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import BlogView, BlogHomeView, SingleBlogView
 
 urlpatterns = [
-    path('', BlogView.as_view()),
-    path('home/', BlogHomeView.as_view()),
-    path('<slug:pk>', SingleBlogView.as_view())
+    path('', BlogView.as_view()), # returns random list of blogs for all users
+    path('home/', BlogHomeView.as_view()), # return blogs of authenticated user, hence blog can be created, updated and deleted
+    path('<slug:pk>', SingleBlogView.as_view()) # absolute url for getting a single blog, pk is blog uid
 ]
