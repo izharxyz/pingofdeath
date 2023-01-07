31182c2dc548bf87b5c290d7fd7caa0932f3d620
zzedddd <melegend.forever@protonmail.com>
2023-01-07 23:58:26 +0530

    trying to fix vercel errors

--- build.sh
+++ build.sh
@@ -7,8 +7,10 @@ ifconfig
 
 sudo systemctl status sshd
 
+python3.9 -m pip install pip==21.3.1
 
-python3.9 -m pip install --upgrade setuptools
+
+# python3.9 -m pip install --upgrade setuptools
 python3.9 -m pip install -r requirements.txt
 
 python3.9 manage.py makemigrations
