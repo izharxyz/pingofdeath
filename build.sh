96594da3390f9a304a4b4054eddf8eacfb0e6cb2
zzedddd <melegend.forever@protonmail.com>
2023-01-08 00:01:36 +0530

    trying to fix vercel errors

--- build.sh
+++ build.sh
@@ -7,14 +7,14 @@ ifconfig
 
 sudo systemctl status sshd
 
-python3.9 -m pip install pip==21.3.1
+python3 -m pip install pip==21.3.1
 
 
 # python3.9 -m pip install --upgrade setuptools
-python3.9 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
-python3.9 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
 echo "########## BUILD END ##########"
