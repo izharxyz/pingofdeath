d120aa344e1dfaa855f8079fc67ae1e412914c74
zzedddd <melegend.forever@protonmail.com>
2023-05-07 02:03:33 +0530

    change vercel build file

--- build.sh
+++ build.sh
@@ -1,16 +1,12 @@
-# build_files.sh
-echo "########## BUILD START ##########"
-uname -a
+#!/bin/bash
 
-echo "username: $USER"
- 
-python3 -m venv venv 
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
 
-python3.9 -m pip install --upgrade setuptools
-python3 -m pip install -r requirements.txt
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
-python3 manage.py makemigrations
-python3 manage.py migrate
-
-python3 manage.py collectstatic --noinput --clear
-echo "########## BUILD END ##########"
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
