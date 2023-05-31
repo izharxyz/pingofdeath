a33cdfd1c976a08f3d20cc8881aa4cc3c43796ca
zzedddd <melegend.forever@protonmail.com>
2023-06-01 01:26:23 +0530

    remove unused imports

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
