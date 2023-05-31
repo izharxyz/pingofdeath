434bccff8dbae0003285d6e09a7807a7e13eac5c
zzedddd <melegend.forever@protonmail.com>
2023-06-01 01:21:23 +0530

    configure static files for vercel

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +11,5 @@ urlpatterns = [
     path('blog/', include('blog.urls'))
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
