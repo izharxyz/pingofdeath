ad7491e5dd0dcf9a35c9c33ecfe4d850b2bfb8d8
zzedddd <melegend.forever@protonmail.com>
2023-09-15 23:09:04 +0530

    configured debug mode in settings

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f2h%6s9*nu1a133w7(coo6pgbvdzp=0j(*8v0fazracwj*x5^u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
