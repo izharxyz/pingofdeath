f48541094d32fcd27a9cc9199e965248972e2d92
zzedddd <melegend.forever@protonmail.com>
2023-09-18 00:30:43 +0530

    moved urls to index app

--- index/urls.py
+++ index/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import include, path
 
 from .views import IndexView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+
+    path('api/accounts/', include('authapi.urls')),
+    path('api/blog/', include('blog.urls'))
 ]
